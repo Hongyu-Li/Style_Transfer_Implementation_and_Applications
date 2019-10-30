210cb4175eedddebbe1181e52395032c83f913a1
hongyu.li <hongyu.li@thoughtworks.com>
2019-10-30 21:37:00 +0800

    [Hongyu] deploy dash to gcp

--- dashapp/Index.py
+++ dashapp/Index.py
@@ -6,9 +6,20 @@ from dash.dependencies import Input, Output
 import tensorflow as tf
 tf.enable_eager_execution()   
 
-from App import app
-
+import dash
 import Home, DIY, WebCam, About
+
+external_scripts = ["https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+                    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"]
+
+external_stylesheets = ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+                        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"]
+
+app = dash.Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.config.suppress_callback_exceptions = True
+app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
