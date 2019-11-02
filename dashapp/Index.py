2c84f3485ff724a42a9711105efd0b4522849a55
hongyu.li <hongyu.li@thoughtworks.com>
2019-11-02 11:56:51 +0800

    [Hongyu] revise yaml configuration

--- dashapp/Index.py
+++ dashapp/Index.py
@@ -1,25 +1,13 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from App import app
 
 #Eager Mode
 import tensorflow as tf
 tf.enable_eager_execution()   
 
-import dash
 import Home, DIY, WebCam, About
-
-external_scripts = ["https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
-                    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"]
-
-external_stylesheets = ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
-                        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"]
-
-app = dash.Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets)
-server = app.server
-
-app.config.suppress_callback_exceptions = True
-app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
